5cb28ed2dca808e0a1949a035e95a9d4996c7b29
amin <aminzabardast@gmail.com>
2019-03-29 14:05:48 +0300

    First build

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
+    <v-content style="background: #dc7ded">
+      <Logo/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Logo from './components/Logo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Logo
   },
   data () {
     return {
---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container bg fill-height text-xs-center>
+        <v-layout align-center justify-center wrap column>
+            <v-flex xs1>
+                <p class="display-3 logo">Amin Zabardast</p>
+            </v-flex>
+            <v-flex xs1>
+                <p class="subheading">This is empty for now</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Logo"
+    }
+</script>
+
+<style scoped>
+    .logo {
+        font-family: 'Satisfy', cursive !important;
+    }
+</style>
